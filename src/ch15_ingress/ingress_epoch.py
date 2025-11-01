ea5416c3ec50096a6dee1b83b77bea67781281fb
Joachim <jschalk@users.noreply.github.com>
2025-11-01 16:54:25 -0700

    #1012 replace "forge" with "ingress"

--- src/ch15_ingress/ingress_epoch.py
+++ src/ch15_ingress/ingress_epoch.py
@@ -12,11 +12,11 @@ from src.ch04_rope.rope import (
     get_parent_rope,
     get_tail_label,
 )
-from src.ch15_forge._ref.ch15_semantic_types import EpochTime, FaceName, SparkInt
+from src.ch15_ingress._ref.ch15_semantic_types import EpochTime, FaceName, SparkInt
 
 
 @dataclass
-class EpochForge:
+class EpochIngress:
     face_name: FaceName = None
     spark_num: SparkInt = None
     # otx2inx dict key 'otx_epoch_length', key can be None is only inx_epoch_diff is given
@@ -62,35 +62,37 @@ class EpochForge:
         }
 
 
-def epochforge_shop(
+def epochingress_shop(
     face_name: FaceName,
     spark_num: SparkInt = None,
     otx2inx: dict[EpochTime, EpochTime] = None,
 ):
-    x_epochforge = EpochForge(face_name=face_name, spark_num=get_0_if_None(spark_num))
-    x_epochforge.set_all_otx2inx(otx2inx)
-    return x_epochforge
+    x_epochingress = EpochIngress(
+        face_name=face_name, spark_num=get_0_if_None(spark_num)
+    )
+    x_epochingress.set_all_otx2inx(otx2inx)
+    return x_epochingress
 
 
-def get_epochforge_from_dict(x_dict: dict) -> EpochForge:
-    return epochforge_shop(
+def get_epochingress_from_dict(x_dict: dict) -> EpochIngress:
+    return epochingress_shop(
         face_name=x_dict.get("face_name"),
         spark_num=x_dict.get("spark_num"),
         otx2inx=x_dict.get("otx2inx"),
     )
 
 
-class inherit_epochforgeException(Exception):
+class inherit_epochingressException(Exception):
     pass
 
 
-def inherit_epochforge(new: EpochForge, old: EpochForge):
+def inherit_epochingress(new: EpochIngress, old: EpochIngress):
     if new.face_name != old.face_name:
         exception_str = "Core attrs in conflict"
-        raise inherit_epochforgeException(exception_str)
+        raise inherit_epochingressException(exception_str)
     if new.spark_num <= old.spark_num:
-        exception_str = "older EpochForge is not older"
-        raise inherit_epochforgeException(exception_str)
+        exception_str = "older EpochIngress is not older"
+        raise inherit_epochingressException(exception_str)
     for otx_epoch_length, inx_epoch_diff in old.otx2inx.items():
         if not new.otx_exists(otx_epoch_length):
             new.set_otx2inx(otx_epoch_length, inx_epoch_diff)
