73682a4c2cf3c6f7f435a29dbd2cdaf70c5cb150
Joachim <jschalk@users.noreply.github.com>
2026-03-18 06:43:45 -0600

    #1069 create /today.py file

--- today.py
+++ today.py
@@ -1,29 +1,11 @@
-# test_file_path = "src\ch18_world_etl\test\z_heard\test_heard_agg_update_reasonnum_columns.py"
-# test_name = "test_get_update_prncase_inx_epoch_diff_sqlstr_SetsColumnValues"
-
-# dest_dir = "src/ch18_world_etl/test/zz_notebooks"
-# dest_filename = "reasonnum_update_test.py"
-# from src.ch00_py.file_toolbox import create_path
-
-# dest_file_path = create_path(dest_dir, dest_filename)
-# from src.ch00_py.notebook_toolbox import save_marimo_notebook_from_test_file
-
-# save_marimo_notebook_from_test_file(test_file_path, test_name, dest_file_path)
-
-
 from os import getcwd as os_getcwd
 from os.path import isdir as os_path_isdir
-from src.ch00_py.file_toolbox import create_path, open_json, save_json
-from src.ch00_py.notebook_toolbox import save_marimo_notebook_from_test_file
-from src.linter.chapter_migration_tools import (
-    first_level_dirs_with_prefix,
-    rename_files_and_dirs_4times,
-    string_exists_in_filepaths,
-)
+from src.ch00_py.file_toolbox import create_path, open_json, save_file
+from src.ch19_world_kpi.gcalendar import get_gcal_day_report
 
 # HOW TO USE:
 # Open up CMD, change directory to repo
-# Enter this: python -m src.linter.create_notebook -x
+# Enter this: python today.py
 
 
 def main():
@@ -41,7 +23,7 @@ def main():
     print(f"{dest_file_path=}")
     print("")
     print(f"marimo edit {dest_file_path}")
-    save_marimo_notebook_from_test_file(test_file_path, test_name, dest_file_path)
+    # save_marimo_notebook_from_test_file(test_file_path, test_name, dest_file_path)
 
     # find_str = input("Find string: ").strip()
     # replace_str = input("Replace string: ").strip()
